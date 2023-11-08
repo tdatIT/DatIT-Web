ce88759c253e3d4b682494a01e2bc525a6b17d52
tdatIT <tdat.it2k2@gmail.com>
2023-11-08 13:07:16 +0700

    confirm download cv

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,3 +19,16 @@ tabs.forEach(tab => {
     })
 })
 
+$("#confirm-btn").on('click', function () {
+    let input = $("#confirm-input").val()
+    if (input === 'hello') {
+        $("#confirm-input").val("")
+        link = document.createElement("a");
+        link.setAttribute("href", "/assets/pdf/CV-TienDat-Backend-10-2023.pdf");
+        link.setAttribute("download", "CV-TienDat-Backend-10-2023.pdf");
+        link.click();
+        link.remove()
+        MicroModal.close('modal-2')
+    }
+})
+
